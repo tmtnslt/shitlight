5261a620369b27e356c7c8b8f39061c40e762b34
Marc Wiedermann <marcwiedermann@posteo.de>
2016-04-15 02:15:16 +0200

    driver for 3 shift register

---
+++ test-3-shiftregister.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import time
+import numpy as np
+
+# Define pins
+_SER_pin   = 25   #pin 14 on the 75HC595 
+_RCLK_pin  = 24   #pin 12 on the 75HC595
+_SRCLK_pin = 23   #pin 11 on the 75HC595
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False) 
+
+GPIO.setup(_SER_pin, GPIO.OUT)
+GPIO.setup(_RCLK_pin, GPIO.OUT)
+GPIO.setup(_SRCLK_pin, GPIO.OUT)
+
+brights = np.ones(24) * 64
+brights = np.array(brights)
+n_channel = len(brights)
+
+# Reset all LED
+for i in range(n_channel-1, -1, -1):
+    GPIO.output(_SER_pin, GPIO.LOW)
+    GPIO.output(_SRCLK_pin, GPIO.HIGH) 
+    GPIO.output(_SRCLK_pin, GPIO.LOW) 
+GPIO.output(_RCLK_pin, GPIO.HIGH) 
+GPIO.output(_RCLK_pin, GPIO.LOW) 
+
+time.sleep(0.5)
+GPIO.output(_SER_pin, GPIO.LOW)
+GPIO.output(_SRCLK_pin, GPIO.HIGH) 
+GPIO.output(_SRCLK_pin, GPIO.LOW) 
+    
+GPIO.output(_SER_pin, GPIO.HIGH)
+GPIO.output(_SRCLK_pin, GPIO.HIGH) 
+GPIO.output(_SRCLK_pin, GPIO.LOW) 
+GPIO.output(_RCLK_pin, GPIO.HIGH) 
+GPIO.output(_RCLK_pin, GPIO.LOW) 
+time.sleep(0.5)
+GPIO.output(_SER_pin, GPIO.LOW)
+
+for _ in range(24):
+    GPIO.output(_SRCLK_pin, GPIO.HIGH) 
+    GPIO.output(_SRCLK_pin, GPIO.LOW) 
+    GPIO.output(_RCLK_pin, GPIO.HIGH) 
+    GPIO.output(_RCLK_pin, GPIO.LOW) 
+    time.sleep(0.5)
+
+
--- test-frequency.py
+++ test-frequency.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import numpy as np
+import sys
 
 # Define pins
 _SER_pin   = 25   #pin 14 on the 75HC595
@@ -14,60 +15,62 @@ GPIO.setup(_SER_pin, GPIO.OUT)
 GPIO.setup(_RCLK_pin, GPIO.OUT)
 GPIO.setup(_SRCLK_pin, GPIO.OUT)
 
-times = []
-#brights = [0, 0, 0, 0 ,0, 0, 0, 0, 0, 0, 200, 0, 0, 0, 0, 0]
-brights = np.ones(16) * 64
-brights = np.array(brights)
-n_channel = len(brights)
+n_channel = 24
 
-# Reset all LED
-for i in range(n_channel-1, -1, -1):
-    GPIO.output(_SER_pin, GPIO.LOW)
-    GPIO.output(_SRCLK_pin, GPIO.HIGH) 
-    GPIO.output(_SRCLK_pin, GPIO.LOW) 
-GPIO.output(_RCLK_pin, GPIO.HIGH) 
-GPIO.output(_RCLK_pin, GPIO.LOW) 
+def reset():
+    for i in range(n_channel-1, -1, -1):
+        GPIO.output(_SER_pin, GPIO.LOW)
+        GPIO.output(_SRCLK_pin, GPIO.HIGH) 
+        GPIO.output(_SRCLK_pin, GPIO.LOW) 
+    GPIO.output(_RCLK_pin, GPIO.HIGH) 
+    GPIO.output(_RCLK_pin, GPIO.LOW) 
 
-time.sleep(0.25)
-#GPIO.output(_SER_pin, GPIO.LOW)
-##GPIO.output(_SRCLK_pin, GPIO.HIGH) 
-##GPIO.output(_SRCLK_pin, GPIO.LOW) 
-#    
-#GPIO.output(_SER_pin, GPIO.HIGH)
-#GPIO.output(_SRCLK_pin, GPIO.HIGH) 
-#GPIO.output(_SRCLK_pin, GPIO.LOW) 
-#GPIO.output(_RCLK_pin, GPIO.HIGH) 
-#GPIO.output(_RCLK_pin, GPIO.LOW) 
-#time.sleep(0.25)
-#GPIO.output(_SER_pin, GPIO.LOW)
-#
-#for _ in range(15):
-#    GPIO.output(_SRCLK_pin, GPIO.HIGH) 
-#    GPIO.output(_SRCLK_pin, GPIO.LOW) 
-#    GPIO.output(_RCLK_pin, GPIO.HIGH) 
-#    GPIO.output(_RCLK_pin, GPIO.LOW) 
-#    time.sleep(0.25)
-#
-##GPIO.output(_RCLK_pin, GPIO.LOW) 
-#print n_channel
-#
-#GPIO.output(_SRCLK_pin, GPIO.LOW) 
-#GPIO.output(_SER_pin, GPIO.HIGH)
+reset()
 
 while True:
-    for bit in range(0, 256, 8):
-        mask = brights <= bit
-        for i in range(n_channel-1, -1, -1):
-            if mask[i] == True:
-                GPIO.output(_SER_pin, GPIO.LOW)
-            else:
-                GPIO.output(_SER_pin, GPIO.HIGH)
+    try:
+        for l in range(3):
+            for j in range(8):
+                brights = np.zeros(n_channel)
+                brights[l+j*3] = 32 * (j + 1) - 1
 
-            GPIO.output(_SRCLK_pin, GPIO.HIGH) 
-            GPIO.output(_SRCLK_pin, GPIO.LOW) 
+                for _ in range(8):
+                    for bit in range(0, 256, 8):
+                        mask = brights <= bit
+                        for i in range(n_channel-1, -1, -1):
+                            if mask[i] == True:
+                                GPIO.output(_SER_pin, GPIO.LOW)
+                            else:
+                                GPIO.output(_SER_pin, GPIO.HIGH)
+
+                            GPIO.output(_SRCLK_pin, GPIO.HIGH) 
+                            GPIO.output(_SRCLK_pin, GPIO.LOW) 
+
+                        GPIO.output(_RCLK_pin, GPIO.HIGH) 
+                        GPIO.output(_RCLK_pin, GPIO.LOW) 
+
+            for j in range(8):
+                brights = np.zeros(n_channel)
+                brights[l+j*3] = 32 * (j + 1) - 1
+                brights[((l+1)%3)+j*3] = 32 * (j+1) - 1
+
+                for _ in range(8):
+                    for bit in range(0, 256, 8):
+                        mask = brights <= bit
+                        for i in range(n_channel-1, -1, -1):
+                            if mask[i] == True:
+                                GPIO.output(_SER_pin, GPIO.LOW)
+                            else:
+                                GPIO.output(_SER_pin, GPIO.HIGH)
+
+                            GPIO.output(_SRCLK_pin, GPIO.HIGH) 
+                            GPIO.output(_SRCLK_pin, GPIO.LOW) 
+
+                        GPIO.output(_RCLK_pin, GPIO.HIGH) 
+                        GPIO.output(_RCLK_pin, GPIO.LOW) 
 
 
-        GPIO.output(_RCLK_pin, GPIO.HIGH) 
-        GPIO.output(_RCLK_pin, GPIO.LOW) 
-        
-print np.mean(times)
+    except KeyboardInterrupt:
+        reset()
+        sys.exit()
+        print "Exiting"
